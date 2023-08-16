59bedfcbf58e234e490013cade1c4a7bddee8a10
Azher iqbal <azheriqbal742@hotmail.com>
2023-08-16 16:35:37 +0500

    not in titnalocn

---
+++ Dsa/RemoveElement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsa
+{
+    public class RemoveElement
+    {
+        public static int RemoveEl(int[] num, int val)
+        {
+            int count = 0;
+            for(var i=0; i<num.Length;i++)
+            {
+                if(num[i] != val)
+                {
+                    num[count] = num[i];
+                    count++;
+                }
+               
+            }
+
+            return count;
+        }
+    }
+}
---
+++ TestCases/RemoveDuplicateTests.cs
@@ -0,0 +1,22 @@
+﻿using Dsa;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCases
+{
+    public class RemoveDuplicateTests
+    {
+        [TestCase(new int[] { 1, 1, 2 }, ExpectedResult = 2)]
+        [TestCase(new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 }, ExpectedResult = 5)]
+        [TestCase(new int[] { 1, 2, 3, 4, 5 }, ExpectedResult = 5)]
+        [TestCase(new int[] { 1, 1, 1, 1, 1 }, ExpectedResult = 1)]
+        [TestCase(new int[] { }, ExpectedResult = 0)]
+        public int RemoveDuplicates_ShouldReturnLengthOfUniqueElements(int[] nums)
+        {
+            return RemoveDuplicate.RemoveDuplicates(nums);
+        }
+    }
+}
