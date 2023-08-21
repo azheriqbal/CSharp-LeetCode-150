75fcea7222137346e49798b168421c68276d2f2c
Azher iqbal <azheriqbal742@hotmail.com>
2023-08-21 12:38:13 +0500

    LinkedList

--- Dsa/Program.cs
+++ Dsa/Program.cs
@@ -2,6 +2,55 @@
 using Dsa;
 using LeetCode;
 using System.Text;
+using static LeetCode.LinkedListS;
 
-int[] nums = { 2, 3, 4 };
-int target = 6;
+string s = "a-bC-dEf-ghIj";
+
+char[] sa = s.ToCharArray();
+
+int l = 0;
+int r =sa.Length-1;
+
+while(l < r)
+{
+    if (char.IsLetter(sa[r]) && char.IsLetter(sa[l]))
+    {
+        char tem = sa[l];
+        sa[l] = sa[r];
+        sa[r]=tem;
+        l++;
+        r--;
+    }
+
+}
+
+foreach(char c in sa)
+{
+    Console.WriteLine(c);
+}
+
+/*ListNode node1 = new ListNode(1);
+ListNode node2 = new ListNode(2);
+ListNode node3 = new ListNode(3);
+ListNode node4 = new ListNode(4);
+
+
+// Connecting the nodes to form a linked list
+node1.Next = node2;
+node2.Next = node3;
+node3.Next = node4;
+
+// Creating a cycle by making the last node point to the second node
+node4.Next = node2;
+
+// Create an instance of the Solution class
+
+// Call the HasCycle method to check if a cycle exists
+bool hasCycle = LinkedListS.HasCycle(node1);
+
+Console.WriteLine("Cycle detected: " + hasCycle);*/
+
+
+/*int[] arr = { 2, 3, 4, 10, 40 };
+int res = SearchAlogs.BinarySearch(arr, 40);
+Console.WriteLine(res);*/
---
+++ Dsa/SearchAlogs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class SearchAlogs
+    {
+        public static int BinarySearch(int[] array, int target)
+        {
+            int left = 0;
+            int right = array.Length - 1;
+
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+
+                if (array[mid] == target)
+                    return mid; // Target found at index mid
+
+                if (array[mid] < target)
+                    left = mid + 1; // Search in the right half
+                else
+                    right = mid - 1; // Search in the left half
+            }
+
+            return -1; // Target not found
+        }
+
+    }
+}
